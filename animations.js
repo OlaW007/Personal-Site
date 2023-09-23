154701a21c97aa7a10d834c25aff7ecc2e411467
OlaW007 <128760313+OlaW007@users.noreply.github.com>
2023-09-24 01:53:54 +0200

    Update animations.js

--- animations.js
+++ animations.js
@@ -1,31 +1,37 @@
-// Define an array of selectors
-const selectors = [
-  '#box',
-  'button',
-  '#portrait-div',
-  '#nadpis',
-  '#ib',
-  '#ib-text',
-  '#experience-header',
-  '.category1>.polozka',
-  '.category2>.polozka',
-  '.category3>.polozka'
+// Define selectors with their respective thresholds
+const selectorsWithThreshold = [
+  { selector: '#box', threshold: 0 },
+  { selector: 'button', threshold: 0 },
+  { selector: '#portrait-div', threshold: 0 },
+  { selector: '#nadpis', threshold: 0 },
+  { selector: '#ib', threshold: 0 },
+  { selector: '#ib-text', threshold: 0.048},
+  { selector: '.category1>.polozka', threshold: 0 },
+  { selector: '.category2>.polozka', threshold: 0 },
+  { selector: '.category3>.polozka', threshold: 0 },
+  { selector: '#experience-header', threshold: 0.95 },
+  
 ];
 
-// Create an intersection observer
-let observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    // If entry (element) is in view
-    if (entry.isIntersecting) {
-      entry.target.classList.add("loaded");
-      observer.unobserve(entry.target);
-    }
-  });
+// Iterate over the array and create an observer for each item
+selectorsWithThreshold.forEach(({ selector, threshold }) => {
+  const elements = document.querySelectorAll(selector);
+  if (elements.length === 0) return;
+
+  let observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        if (entry.target.id === 'experience-header') {
+          entry.target.querySelectorAll('.pismeno').forEach(child => child.classList.add("loaded"));
+        } else {
+          entry.target.classList.add("loaded");
+        }
+        observer.unobserve(entry.target);
+      }
+    });
+  }, { threshold });
+
+  elements.forEach(element => observer.observe(element));
 });
 
-// Iterate over the selectors and observe each element
-selectors.forEach(selector => {
-  document.querySelectorAll(selector).forEach(currentlyObservedElement => {
-    observer.observe(currentlyObservedElement);
-  });
-});
+
